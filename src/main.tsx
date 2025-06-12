248c7d7552629caae989dccff517ea907f51f9b1
mahmoudg.dev <mahomudg.dev@gmail.com>
2025-06-12 17:55:48 +0300

    fix issues for productions 3

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 import { store } from "./redux/index";
 import { Provider } from "react-redux";
 import "./index.css";
